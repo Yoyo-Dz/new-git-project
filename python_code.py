bb310a958e0dae49e2de458cba33bcec478a29f9
Yoyo-Dz <zy4live@outlook.com>
2021-02-05 18:27:59 +0100

    i fix the main function

--- python_code.py
+++ python_code.py
@@ -1,5 +1,8 @@
 def small_function(msg):
   print("the msg that you have enter is " + msg + " end he has {} letter".format(len(msg)))
 
-def main():
+
+
+
+if __name__ == '__main__':
     small_function("can i play football")
