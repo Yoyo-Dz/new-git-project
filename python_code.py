5f82429df312fbc25670ecc9b62f52ea84a7fd9b
Yoyo-Dz <zy4live@outlook.com>
2021-02-05 18:30:17 +0100

    i add new function that prin t n number

--- python_code.py
+++ python_code.py
@@ -1,8 +1,12 @@
 def small_function(msg):
   print("the msg that you have enter is " + msg + " end he has {} letter".format(len(msg)))
 
+def print_10number(num):
+    for i in range(num):
+        print(i)
 
 
 
 if __name__ == '__main__':
     small_function("can i play football")
+    print_10number(10)
